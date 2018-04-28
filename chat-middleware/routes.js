09631835716299798d4246748fb50b09d6cee6b4
Elliott Gorrell <elliottgorrell1@gmail.com>
2018-04-28 14:24:28 +1000

    route for receiving message

--- chat-middleware/routes.js
+++ chat-middleware/routes.js
@@ -2,7 +2,16 @@ module.exports = (app) => {
   // Monkey patch express so we can use async/await
   require('express-async-await')(app)
 
-  app.post('/receive-slack', async function(req, res, next) => {
-    //TODO
+  app.post('/slack/message', async function(req, res, next) {
+    // Slack will send us a challenge on first contact to verify our api exists
+    // We just need to echo this back to them
+    if (req.body.challenge) return res.status(200).json({challenge: req.body.challenge});
+
+    let eventType = req.body.event.type;
+
+    if (eventType === 'message'){
+      console.log(`received message: ${req.body.event.text}`);
+    }
   });
+
 }
